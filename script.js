c92c6d83abb98d6fa980f3e31aadc25e6c41c365
EvyBettina <evybettina@hotmail.com>
2024-10-04 11:46:56 +0200

    js check consol log

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 let userInput = document.querySelector("#user-input");
 console.log(userInput)
 let todoListElement = document.querySelector("#todo-list");
+
+function handleSubmit(event) {
+event.preventDefault();
+console.log(event);
+console.log("Form Submitted");
+
+userInput.addEventListener("submit", handleSubmit);
