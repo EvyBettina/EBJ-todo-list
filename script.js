d2086cd62efb4a305bc84c6393b0a81c42f51531
EvyBettina <evybettina@hotmail.com>
2024-10-09 13:55:45 +0200

    added function renderTodos

--- script.js
+++ script.js
@@ -16,78 +16,93 @@ console.log(converted); */
 let userInput = document.querySelector("#user-input");
 let todoListElement = document.querySelector("#todo-list");
 
+userInput.addEventListener("submit", handleSubmit);
+
 let storedTodos = localStorage.getItem("todos");
 let convertedTodos = JSON.parse(storedTodos);
+
+localStorage.removeItem("todos");
 
 let todos;
 if (storedTodos === null) {
   // Hvis det er første besøk på siden, lag en ny liste
-todos = [];
+  todos = [];
 } else {
   // Hvis det var noe lagret i LocalStorage, bruk det
   todos = convertedTodos;
 }
 
-renderTodos ();
+renderTodos();
 
-// Dette er det som skal skje når brukeren trykker på 
+// Dette er det som skal skje når brukeren trykker på
 // Legg til knappen
 function handleSubmit(event) {
-event.preventDefault(); // Forhindrer nettsiden og lastes inn på nytt (refresh)
+  event.preventDefault(); // Forhindrer nettsiden og lastes inn på nytt (refresh)
 
-console.log("Creating Todo Object...");
-let newTodo = createTodoObject(userInput);
+  console.log("Creating Todo Object...");
+  let newTodo = createTodoObject(userInput);
 
-console.log(todos);
-todos.push(newTodo);
+  console.log(todos);
+  todos.push(newTodo);
 
-console.log("Updating the stored list...");
-let jsonTodos = JSON.stringify(todos)
-localStorage.setItem("todos", jsonTodos);
-console.log(jsonTodos);
+  console.log("Updating the stored list...");
+  let jsonTodos = JSON.stringify(todos);
+  localStorage.setItem("todos", jsonTodos);
+  console.log(jsonTodos);
 
-console.log("Clearing out the old todos from the document...");
-todoListElement.innerHTML = "";
+  console.log("Clearing out the old todos from the document...");
+  todoListElement.innerHTML = "";
 
-
-console.log("Appending all todos to the Document...");
-todos.forEach((todo, index) => {
+  console.log("Appending all todos to the Document...");
+  todos.forEach((todo, index) => {
     console.log(todo);
     console.log(index);
-// Først lage det HTML for det gjøremålet
-let newTodoCard = createTodoCard(todo);
+    // Først lage det HTML for det gjøremålet
+    let newTodoCard = createTodoCard(todo);
 
-// Legg det nye html element til i Dokumentet
-todoListElement.append(newTodoCard);
-});
-    
+    // Legg det nye html element til i Dokumentet
+    todoListElement.append(newTodoCard);
+  });
 }
-
-userInput.addEventListener("submit", handleSubmit);
 
 // Denne leser av daten i et form element
 // og lager et JavaScript objekt for Gjøremålene
 function createTodoObject(form) {
- let todo = form.querySelector("#todo");
-let todoValue = todo.value;
+  let todo = form.querySelector("#todo");
+  let todoValue = todo.value;
 
-let todoObject = {
+  let todoObject = {
     title: todoValue,
   };
 
-return todoObject;
+  return todoObject;
 }
 
 function createTodoCard(todoObject) {
-    // Lage alle elementene vi trenger
- let todoCard = document.createElement("li");
- let titleElement = document.createElement("h2");
+  // Lage alle elementene vi trenger
+  let todoCard = document.createElement("li");
+  let titleElement = document.createElement("h2");
+  let deletebutton = document.createElement("button");
 
-// Sett de sammen til ett element
- todoCard.append(titleElement);
+  // Sett de sammen til ett element
+  todoCard.append(titleElement);
+  todoCard.append(deleteButton);
 
- // Konfigurere elementene med korrekte verdier
- titleElement.textContent = todoObject.title;
+  // Konfigurere elementene med korrekte verdier
+  titleElement.textContent = todoObject.title;
 
-return todoCard;
+  return todoCard;
+}
+// Denne er ansvarlig for å oppdatere HTML
+function renderTodos() {
+  console.log("Appending all todos to the Document...");
+  todos.forEach((todo, index) => {
+    console.log(todo);
+    console.log(index);
+    // Først lage det HTML for det gjøremålet
+    let newTodoCard = createTodoCard(todo);
+
+    // Legg det nye html element til i Dokumentet
+    todoListElement.append(newTodoCard);
+  });
 }
